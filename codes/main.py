de15b02030e6a6b19d91cd493191dfdcf458e8a7
Admin <yeebbb123@gmail.com>
2020-07-03 17:53:03 +0800

    Second progress: only try download left

--- codes/main.py
+++ codes/main.py
@@ -3,65 +3,101 @@ from bs4 import BeautifulSoup
 import requests
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
 
 # redirect to the correct login page
-sess = requests.session()
-sess.headers = {'user-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'}
-url = 'https://learn.polyu.edu.hk/'
-front_page = sess.get(url)
-bs_front_page = BeautifulSoup(front_page.text,'html.parser')
-k = bs_front_page.find_all(href = True)
-Iwant = re.findall('[^\"]*(?:saml|SAML)[^\"]*',k.__str__())[0]
-print("Redirected to {}".format(url+Iwant))
-consent_cookie = {'name':'COOKIE_CONSENT_ACCEPTED','value':'true','Domain':'learn.polyu.edu.hk','expires':'2030-06-19T19:49:48.000Z'}
-
+def redirect():
+    sess = requests.session()
+    sess.headers = {'user-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'}
+    url = 'https://learn.polyu.edu.hk/'
+    front_page = sess.get(url)
+    bs_front_page = BeautifulSoup(front_page.text,'html.parser')
+    k = bs_front_page.find_all(href = True)
+    Iwant = re.findall('[^\"]*(?:saml|SAML)[^\"]*',k.__str__())[0]
+    print("Redirected to {}".format(url+Iwant))
+    return url+Iwant
 
 #login blackboard
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-try:
-    driver = webdriver.Chrome(ChromeDriverManager().install())
-except:
-    print("error in loading driver")
-    exit()
-#driver = webdriver.Chrome("chromedriver_win32/chromedriver.exe")
-driver.get(url+Iwant)
-driver.add_cookie(consent_cookie)
-print("Make sure you can log in blackboard as in your browser.\nThe program can only crawl the courses included in \"My Course\".")
-print(f"If you wish to crawl last few sem courses, please add the courses into \"My Course\" manually (or you can pay me to code more:D).\nThis program is like automating the manual downloading from BC in browser.\nIt is slow sorry.:( ")
-#Lots more improvement can be made especially in finding the target
-while True:
-    username = input("enter student ID: ")
-    password = input("enter password: ")
-    driver.find_element_by_id("userNameInput").send_keys("16061778d")
-    driver.find_element_by_id("passwordInput").send_keys("Azxiop123")
-    driver.find_element_by_id("submitButton").click()
-    if driver.title.find('Welcome') != -1:
-        print('Logged in')
-        break
-    else:
-        print("Failed to log in")
+def loginBD(login_url):
+#    from selenium import webdriver
+#    from webdriver_manager.chrome import ChromeDriverManager
+    global driver
+    try:
+        driver = webdriver.Chrome(ChromeDriverManager().install())
+    except:
+        print("error in loading driver")
+        exit()
+    #driver = webdriver.Chrome("chromedriver_win32/chromedriver.exe")
+    driver.get(login_url)
+    consent_cookie = {'name':'COOKIE_CONSENT_ACCEPTED','value':'true','Domain':'learn.polyu.edu.hk','expires':'2030-06-19T19:49:48.000Z'}
+    print("Make sure you can log in blackboard as in your browser.\nThe program can only crawl the courses included in \"My Course\".")
+    print(f"If you wish to crawl last few sem courses, please add the courses into \"My Course\" manually (or you can pay me to code more:D).\nThis program is like automating the manual downloading from BC in browser.\nIt is slow sorry.:( ")
+    #Lots more improvement can be made especially in finding the target
+    while True:
+        #username = input("enter student ID: ")
+        #password = input("enter password: ")
+        driver.find_element_by_id("userNameInput").send_keys("16061778d")
+        driver.find_element_by_id("passwordInput").send_keys("Azxiop123")
+        driver.find_element_by_id("submitButton").click()
+        if driver.title.find('Welcome') != -1:
+            driver.add_cookie(consent_cookie)
+            driver.refresh()
+            print('Logged in')
+            break
+        else:
+            print("Failed to log in")
 
 #a = driver.find_elements_by_class_name("termToggleLink itemHead")
-sem = driver.find_elements_by_partial_link_text('Sem') + driver.find_elements_by_partial_link_text('sem')
-ava_sem_text = [SEM.text for SEM in sem ]
-sem_id = [SEM.get_attribute("id")[4:] for SEM in sem ]
+
+#+ driver.find_elements_by_partial_link_text('sem')
+#///ava_sem_text = [SEM.text for SEM in sem ]
+#///sem_id = [SEM.get_attribute("id")[4:] for SEM in sem ]
 #get courses of a particular sem link ///need to be def and incoperate
-ID = sem_id[2]
-courses = driver.find_elements_by_xpath("//div[@id='{}']/ul/li".format(ID))
-courses[1].click()
+#return sems info
+def get_sems_info():
+    sems = driver.find_elements_by_partial_link_text('Sem')
+    sems_info = [(SEM.text,SEM.get_attribute("id")[4:]) for SEM in sems ]
+    print("\n".join([i+": "+j for i,j in sems_info]))
+#    print("\n".join(str(i)+": "+j for i,j in enumerate([i[0] for i in sems_info])))
+    return sems_info
+
+#pop unwanted sem
+#///print("\n".join(str(i)+": "+j for i,j in enumerate([i[0] for i in sems_info])))
+def pref_sems_info(sems_info):
+    print("\n".join(str(i) + ": " + j for i, j in enumerate([i[0] for i in sems_info])))
+    while True:
+        index = input("Enter UNWANTED sem index(space separated if multiple): ")
+        swi = input("Confirmed?(Y/N)")
+        if (swi=="Y"):
+            try:
+                k = [int(i) for i in list(index.split())]
+                if not ((max(k) <= len(sems_info)-1) and (min(k)>=0) and (len(k)==len(set(k)))):
+                    raise ValueError
+                break
+            except ValueError:
+                print("Invalid entry. Try again")
+    return [sems_info[i] for i in range(len(sems_info)) if i not in k]
+
+
+#get the courses names and urls in triple form
+#####sems_info = pref_sems_info(sems_info)
+def find_courses_info(pref_sems):
+    IDs = [Sems_info[1] for Sems_info in pref_sems]
+    courses = []
+    for ID in IDs:
+        if not driver.find_element_by_xpath("//div[@id='{}']/ul/li/a".format(ID)).text:
+            driver.find_element_by_id("afor"+ID).click()
+            #put a flag?
+        courses += driver.find_elements_by_xpath("//div[@id='{}']/ul/li/a".format(ID))
+#        courses_info = [(course.text,course.get_attribute("href")) for course in courses]
+        print("\n".join([i.text for i in courses]))
+    return [(course.text,course.get_attribute("href")) for course in courses]
+
+def find_all_materials(courses_info):
+    for cname,curl in courses_info:
 
 
 
-a = driver.find_elements_by_css_selector('a')
-a_text = [A.text for A in a]
-#open close
-li = driver.find_elements_by_css_selector('ul.portletList-img')
-[SEM.click() for SEM in sem]
-li = li + driver.find_elements_by_css_selector('ul.portletList-img')
-[SEM.click() for SEM in sem]
-li_text = [LI.text for LI in li]
-sem[0].get_attribute("id")
 
 
 
--- codes/trylab2.py
+++ codes/trylab2.py
@@ -41,6 +41,7 @@ bs_redir_page.find_all(id = True)
 
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
 driver = webdriver.Chrome(ChromeDriverManager().install())
 #driver = webdriver.Chrome("chromedriver_win32/chromedriver.exe")
 driver.get(url+Iwant)
@@ -59,12 +60,66 @@ enu_sem_text = list(enumerate(ava_sem_text))
 for i,j in enu_sem_text:
     print("{0} : {1}".format(i,j))
 sele_sem = []
+print("\n".join([i+": "+j for i,j in zip(ava_sem_text,sem_id)]))
+
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get("https://www.google.com/search?q=hi&oq=hi&aqs=chrome.0.69i59.413j0j8&sourceid=chrome&ie=UTF-8")
+point = driver.find_element_by_partial_link_text("hi - Urban Dictionary")
+point.get_attribute('href')
+
+
+
+ID = [Sems_info[1] for Sems_info in sems_info][2] #particular id: 2
+courses = driver.find_elements_by_xpath("//div[@id='{}']/ul/li/a".format(ID))
+courses_info = [(course.text,course.get_attribute("href")) for course in courses ]
+print("\n".join([i.text for i in courses]))
+
+
+
+pref_sems = sems_info.copy()
+for i in k:
+    print(i)
+    pref_sems.pop(i-1)
+
+
+
+
+driver.find_element_by_tag_name("body")
+driver.refresh()
+
+pref_sems = sems_info.copy()
+IDs = [Sems_info[1] for Sems_info in pref_sems]
+courses = []
+for ID in IDs:
+    if not driver.find_element_by_xpath("//div[@id='{}']/ul/li/a".format(ID)).text:
+        driver.find_element_by_id("afor"+ID).click()
+        #flag = True
+        print("clicked {}".format(ID))
+    courses += driver.find_elements_by_xpath("//div[@id='{}']/ul/li/a".format(ID))
+#z    if flag:       driver.find_element_by_id("afor" + ID).click()flag = Falseprint("closed")
+#        courses_info = [(course.text,course.get_attribute("href")) for course in courses]
+print("\n".join([i.text for i in courses]))
+i = [(course.text,course.get_attribute("href")) for course in courses]
 
 
 
 
 
+driver.get(i[0][1])
+# id = puller
+header = driver.find_element_by_id("puller")
+if header.get_attribute("class"):
+    header.click()
+header = driver.find_element_by_css_selector('div.navPaletteContent')
+cols = driver.find_elements_by_xpath("//ul[@id='courseMenuPalette_contents']/li/a")
+cols_name = [col.find_element_by_css_selector("span").get_attribute("title") for col in cols]
+cols_info = [(col.find_element_by_css_selector("span").get_attribute("title"),col.get_attribute("href")) for col in cols]
 
-
-
+#1. find a object to download
+#2. try create a download file, with correct name: courses, file name
+#3. download the file
+#4. loop for each courses
+#5. loop for each sem
+#bonus crab videos from ultra
 
